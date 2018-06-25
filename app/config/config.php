fa4cf8f6eb53f4177c42cce5c59a74155196ad4b
IT不倒翁 (Zhao Binyan) <itbudaoweng@gmail.com>
2018-06-25 18:03:36 +0800

    Update config.php
    need file .env

--- app/config/config.php
+++ app/config/config.php
@@ -1,26 +1,59 @@
 <?php
 
 use Phalcon\Config;
+use Phalcon\Config\Adapter\Ini;
 
-return new Config(
-    [
-        'database' => [
-            'adapter'     => 'Mysql',
-            'host'        => 'localhost',
-            'port'        => 3306,
-            'username'    => 'root',
-            'password'    => '',
-            'dbname'      => 'test',
-            'charset'     => 'utf8mb4',
-        ],
-        'application' => [
-            'controllersDir' => __DIR__ . '/../../app/controllers/',
-            'modelsDir'      => __DIR__ . '/../../app/models/',
-            'viewsDir'       => __DIR__ . '/../../app/views/',
-            'librariesDir'   => __DIR__ . '/../../app/libraries/',
-            'cacheDir'       => __DIR__ . '/../../app/storage/cache/',
-            'logDir'         => __DIR__ . '/../../app/storage/logs/',
-            'baseUri'        => '/',
-        ]
-    ]
-);
+class EnvParser
+{
+    private static $instance = null;
+
+    public static function getAll() {
+        if (self::$instance === null) {
+            self::$instance = new Ini(dirname(dirname(__DIR__)) . '/.env');
+        }
+
+        return self::$instance;
+    }
+
+    public static function getItem($key, $default='') {
+        $env = self::getAll();
+        if (isset($env[$key])) {
+            return $env[$key];
+        }
+        return $default;
+    }
+
+    /**
+     * @return Config
+     */
+    public static function getPSConfig() {
+        $config = new Config(
+            [
+                'database' => [
+                    'adapter'     => 'Mysql',
+                    'host'        => EnvParser::getItem('DB_HOST', 'localhost'),
+                    'port'        => EnvParser::getItem('DB_PORT', 3306),
+                    'username'    => EnvParser::getItem('DB_USERNAME', 'root'),
+                    'password'    => EnvParser::getItem('DB_PASSWORD', ''),
+                    'dbname'      => EnvParser::getItem('DB_DATABASE', 'test'),
+                    'charset'     => 'utf8mb4'
+                ],
+                'application' => [
+                    'controllersDir' => __DIR__ . '/../../app/controllers/',
+                    'modelsDir'      => __DIR__ . '/../../app/models/',
+                    'viewsDir'       => __DIR__ . '/../../app/views/',
+                    'librariesDir'     => __DIR__ . '/../../app/libraries/',
+                    'cacheDir'       => __DIR__ . '/../../app/storage/cache/',
+                    'logDir'         => __DIR__ . '/../../app/storage/logs/',
+                    'baseUri'        => '/',
+                ]
+            ]
+        );
+
+        return $config;
+    }
+
+
+}
+
+return EnvParser::getPSConfig();
